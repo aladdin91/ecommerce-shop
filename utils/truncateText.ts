2cdfa80aa3d0355faa7d442a26d5a412f1016428
Alaa Abdelbaki <30771660+aladdin91@users.noreply.github.com>
2023-12-26 23:11:46 +0100

    create product cards

---
+++ utils/truncateText.ts
@@ -0,0 +1,9 @@
+import exp from "constants";
+
+export const truncateText = (str: string) => {
+  if (str.length < 25) {
+    return str;
+  } else {
+    return str.substring(0, 25) + "...";
+  }
+};
