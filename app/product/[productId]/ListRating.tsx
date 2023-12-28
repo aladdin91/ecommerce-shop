6a1dc30e6493067e3bd75f66e91964c1c4420fd6
Alaa Abdelbaki <30771660+aladdin91@users.noreply.github.com>
2023-12-28 22:59:12 +0100

    create Avatar to show it in reviews

--- app/product/[productId]/ListRating.tsx
+++ app/product/[productId]/ListRating.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Avatar from "@/app/components/Avatar";
 import Heading from "@/app/components/Heading";
 import { Rating } from "@mui/material";
 import moment from "moment";
@@ -18,8 +19,8 @@ const ListRating: React.FC<ListRatingProps> = ({ product }) => {
             return (
               <div key={review.id} className="max-w-[300px]">
                 <div className="flex gap-2 item-center">
-                  <div> Avatar</div>
-                  <div className=" font-semibold"> {review?.user.name}</div>
+                  <Avatar src={review?.user.image} />
+                  <div className=" font-semibold"> {review?.user.name}A</div>
                   <div className=" font-light">
                     {moment(review.createdDate).fromNow()}
                   </div>
