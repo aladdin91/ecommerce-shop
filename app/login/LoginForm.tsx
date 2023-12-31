3f906db59d62083937056fad351287dac47a88d6
Alaa Abdelbaki <30771660+aladdin91@users.noreply.github.com>
2024-01-01 00:06:34 +0100

    create log in component

---
+++ app/login/LoginForm.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useState } from "react";
+import Heading from "../components/Heading";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import Input from "../components/inputs/Input";
+import Button from "../components/Button";
+import Link from "next/link";
+import { AiOutlineGoogle } from "react-icons/ai";
+
+const LoginForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: { email: "", password: "" },
+  });
+
+  const inSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+  };
+
+  return (
+    <>
+      <Heading title="Sign in" />
+      <Button
+        outline
+        label="Continue with google"
+        icon={AiOutlineGoogle}
+        onClick={() => {}}
+      />
+      <hr className="bg-slate-300 w-full h-px" />
+
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+        type="password"
+      />
+      <Button
+        label={isLoading ? "Loading" : "Login"}
+        onClick={handleSubmit(inSubmit)}
+      />
+      <p className="text-sm">
+        Do not have an account?{" "}
+        <Link className="underline" href="/register">
+          Sign up
+        </Link>{" "}
+      </p>
+    </>
+  );
+};
+
+export default LoginForm;
