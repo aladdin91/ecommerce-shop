62e26e62c6ecd803ae05647be4a7c0240470ad6f
Alaa Abdelbaki <30771660+aladdin91@users.noreply.github.com>
2024-01-09 19:36:37 +0100

    show orders details in admin dashboard.

--- app/order/[orderId]/OrderDetails.tsx
+++ app/order/[orderId]/OrderDetails.tsx
@@ -6,6 +6,7 @@ import { formatPrice } from "@/utils/formatPrice";
 import { Order } from "@prisma/client";
 import moment from "moment";
 import { MdAccessTimeFilled, MdDeliveryDining, MdDone } from "react-icons/md";
+import OrderItem from "./OrderItem";
 
 interface OrderDetailsProps {
   order: Order;
@@ -76,6 +77,16 @@ const OrderDetails: React.FC<OrderDetailsProps> = ({ order }) => {
       <div>Date: {moment(order.createDate).fromNow()}</div>
       <div>
         <h2 className=" font-semibold mt-4 mb-2">Product ordered</h2>
+        <div className="grid grid-cols-5 text-xs gap-4 pb-2 items-center">
+          <div className="col-span-2 justify-self-start">PRODUCT</div>
+          <div className=" justify-self-center">PRICE</div>
+          <div className=" justify-self-center">QUANTITY</div>
+          <div className=" justify-self-end">TOTAL AMOUNT</div>
+        </div>
+        {order.products &&
+          order.products.map((item) => {
+            return <OrderItem key={item.id} item={item}></OrderItem>;
+          })}
       </div>
     </div>
   );
